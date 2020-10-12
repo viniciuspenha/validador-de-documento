e851ae37438db83309c701bc5dd4cad17dedd839
Vinicius Penha <vinicius.penha@icarros.com.br>
2020-10-12 17:23:44 -0300

    initial comit

---
+++ src/main/java/br/com/viniciuspenha/validadordedocumento/ValidadorDeDocumentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.viniciuspenha.validadordedocumento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorDeDocumentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidadorDeDocumentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/viniciuspenha/validadordedocumento/ValidadorDeDocumentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciuspenha.validadordedocumento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorDeDocumentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
